f106c85f640c51a6a1b19d006b1fa280e940606a
larrywax <giuseppe.danna@prima.it>
2020-07-16 14:55:13 +0200

    scemo

--- yaml/pretty/container.go
+++ yaml/pretty/container.go
@@ -125,9 +125,9 @@ func printEnviron(w writer, v map[string]*yaml.Variable) {
 	for _, k := range keys {
 		v := v[k]
 		if v.Secret == "" {
-      *w.zero = true
+      w.IncludeZero()
 			w.WriteTagValue(k, v.Value)
-      *w.zero = false
+      w.ExcludeZero()
 		} else {
 			w.WriteTag(k)
 			w.IndentIncrease()
